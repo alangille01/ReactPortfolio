83cf83f224b1f39a2c64ae76a99a446865089cc7
Alexis Langille <langille.alexis@gmail.com>
2024-09-22 14:44:26 -0400

    Update Description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -31,8 +31,8 @@ function About() {
       <div className={`px-3 ${showMore === false && "display-none"}`}>
         <h2 className="py-2">About Me</h2>
         <p className="mb-2" id="about-text">
-          {/* Placeholder */}
-        Placeholder
+          {/**Hello world! My name is Alexis and I am fairly new to computer science but thoroughly enjoying it so far. I got interested in it as my current job has a website and employs a good amount of programmers to work in keeping it running. I spend my free time writing poetry, exercising, and binge watching comedy and drama shows. I look forward to graduating my course and starting my journey in my new career! */}
+          Hello world! My name is Alexis and I am fairly new to computer science but thoroughly enjoying it so far. I got interested in it as my current job has a website and employs a good amount of programmers to work in keeping it running. I spend my free time writing poetry, exercising, and binge watching comedy and drama shows. I look forward to graduating my course and starting my journey in my new career! 
         </p>
       </div>
     </section>
