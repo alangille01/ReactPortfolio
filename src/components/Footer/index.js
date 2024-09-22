c6c6168eb980602d22c467b28e481ce5649b0c07
Alexis Langille <langille.alexis@gmail.com>
2024-09-22 14:06:58 -0400

    Create Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function Footer() {
+  const [contactLinks] = useState([
+    // {
+    //   name: "email",
+    //   link: "mailto:langille.alexis@gmail.com",
+    //   icon: "fa-solid fa-envelope",
+    // },
+    // {
+    //   name: "phone",
+    //   link: "tel:+16263771363",
+    //   icon: "fa-solid fa-phone",
+    // },
+    {
+      name: "github",
+      link: "https://github.com/alangille01",
+      icon: "fa-brands fa-github",
+    },
+    {
+      name: "linkedIn",
+      link: "https://www.linkedin.com/in/alexis-l-86b03522a/?originalSubdomain=ca",
+      icon: "fa-brands fa-linkedin",
+    },
+    {
+      name: "email",
+      link: "mailto:langille.alexis@gmail.com",
+      icon: "fa-solid fa-envelope",
+    },
+  ]);
+  return (
+    <div className="flex-row justify-center mt-3 mb-5 container">
+      {contactLinks.map((link) => (
+        <div className="col-2 text-center" key={link.name}>
+          <a
+            href={link.link}
+            className="ml-2 my-1 px-2 py-1 text-dark icons"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className={link.icon}></i>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Footer;
